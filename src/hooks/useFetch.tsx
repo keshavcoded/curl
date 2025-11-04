3643bb65018b8199e40da22429d52f581f15398a
keshavcoded <keshav.shri06@gmail.com>
2025-11-04 15:51:45 +0530

    [feat]: changed the types

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
 
+type FetchOptions = Record<string, unknown>;
 
+export function useFetch<TResponse>(
+  cb: (options: FetchOptions, ...args: unknown[]) => Promise<TResponse>,
 
-const useFetch = (cb, options ={}) => {
+  options: FetchOptions = {}
+) {
+  const [data, setData] = useState<TResponse | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | Error | null>(null);
 
+  const fn = async (...args: unknown[]) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await cb(options, ...args);
+      setData(response);
+      setError("");
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error, fn };
 }
