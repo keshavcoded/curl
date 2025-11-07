e0545b6a2e07768309d0f6788ecd4d3cb54aa5c7
keshavcoded <keshav.shri06@gmail.com>
2025-11-07 23:54:15 +0530

    fix: error handling and login with google

--- src/lib/api/auth.api.ts
+++ src/lib/api/auth.api.ts
@@ -1,24 +1,86 @@
-import supabase from "@/db/supabase";
+import supabase, { supabaseUrl } from "@/db/supabase";
+import type { FormDataTypes, SignupProps } from "../types";
 
-type LoginParams = {
-  email: string;
-  password: string;
+export const login = async ({ email, password }: FormDataTypes) => {
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) throw new Error(error.message);
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
 };
 
-export const login = async ({ email, password }: LoginParams) => {
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
+export const loginWithGoogle = async () => {
+  try {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
 
-  if (error) throw new Error(error.message);
+    if (error) throw new Error(error.message);
 
-  return data;
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
 };
 
 export const getCurrentUser = async () => {
-  const { data, error } = await supabase.auth.getSession();
-  if (!data.session) return null;
-  if (error) throw new Error(error.message);
-  return data.session?.user;
+  try {
+    const { data, error } = await supabase.auth.getSession();
+    if (!data.session) return null;
+    if (error) throw new Error(error.message);
+    return data.session?.user;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+};
+
+export const signup = async ({
+  name,
+  email,
+  password,
+  profile_img,
+}: SignupProps) => {
+  try {
+    const filename = `img-${name.split(" ")[0]}-${Math.random()}`;
+
+    const { error: uploadError } = await supabase.storage
+      .from("profile_img")
+      .upload(filename, profile_img);
+
+    if (uploadError) throw new Error(uploadError.message); //TODO: add react hot toast
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          name,
+          profile_img: `${supabaseUrl}/storage/v1/public/profile_img/${filename}`,
+        },
+      },
+    });
+
+    if (error) throw new Error(error.message); //TODO: add react hot toast
+
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
 };
