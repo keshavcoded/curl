bb2a7732e6dbf0457e382bf8004a96e986be16d1
keshavcoded <keshav.shri06@gmail.com>
2025-11-17 22:32:59 +0530

    feat: url types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -19,3 +19,14 @@ export type AppContextTypes = {
   isAuthenticated: boolean;
   fetchuser: () => void;
 };
+
+export type UrlTypes = {
+  id: string;
+  title: string;
+  primary_url: string;
+  short_url: string;
+  custom_url: string;
+  qrcode: string;
+  user_id: string;
+  created_at: string;
+};
