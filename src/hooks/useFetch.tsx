16bd57c846fb8a5fc57a42d794d611846b078e79
keshavcoded <keshav.shri06@gmail.com>
2025-11-17 14:06:20 +0530

    feat: allow multiple args and type safety

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,25 +1,21 @@
 import { useState } from "react";
 
-/* type FetchOptions = Record<string, unknown>; */
-
-export function useFetch<TArgs = void, TResponse = unknown>(
-  cb: (args: TArgs) => Promise<TResponse>
-
-  /* options: FetchOptions = {} */
+export function useFetch<TArgs extends unknown[], TResponse>(
+  cb: (...args: TArgs) => Promise<TResponse>
 ) {
   const [data, setData] = useState<TResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
-  const fn = async (args?: TArgs) => {
+  const fn = async (...args: TArgs) => {
     setLoading(true);
     setError(null);
-
     try {
-      const response = await cb(args as TArgs);
-      setData(response);
-    } catch (error) {
-      if (error instanceof Error) setError(error.message);
+      const res = await cb(...args);
+      setData(res);
+    } catch (err) {
+      if (err instanceof Error) setError(err.message);
+      else setError("Unknown error");
     } finally {
       setLoading(false);
     }
