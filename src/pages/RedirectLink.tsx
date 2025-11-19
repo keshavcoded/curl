34acd974e73a65eb47015b40b3af002fce8e4bac
keshavcoded <keshav.shri06@gmail.com>
2025-11-19 22:27:42 +0530

    feat: Redirect link page

---
+++ src/pages/RedirectLink.tsx
@@ -0,0 +1,37 @@
+import { useFetch } from "@/hooks/useFetch";
+import { getAnalytics, getRedirect } from "@/lib/api/url.api";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { SkewLoader } from "react-spinners";
+
+const RedirectLink = () => {
+  const { id } = useParams();
+
+  const { loading, data, fn: redirect } = useFetch(getRedirect);
+
+  const { loading: analyticsloader, fn: saveAnalytics } =
+    useFetch(getAnalytics);
+
+  useEffect(() => {
+    if (!loading) redirect(id);
+  }, []);
+
+  useEffect(() => {
+    if (!loading && data) {
+      saveAnalytics({
+        id: data?.id,
+        primaryUrl: data?.primary_url,
+      });
+    }
+  }, [loading]);
+
+  if (loading || analyticsloader) {
+    return (
+      <div className="z-50 inset-0 flex items-center justify-center fixed bg-black/30 bg-blur-md">
+        <SkewLoader color="#888" />
+      </div>
+    );
+  }
+};
+
+export default RedirectLink;
