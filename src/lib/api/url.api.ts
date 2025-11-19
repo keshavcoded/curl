adb69f3e8fa1a12403119dd8fa0da4c12e18a117
keshavcoded <keshav.shri06@gmail.com>
2025-11-19 22:27:08 +0530

    feat: redirect and store analytics

--- src/lib/api/url.api.ts
+++ src/lib/api/url.api.ts
@@ -1,5 +1,6 @@
 import supabase, { supabaseUrl } from "@/db/supabase";
 import type { createUrlTypes } from "../types";
+import { UAParser } from "ua-parser-js";
 
 export async function getUrls(user_id: string | undefined) {
   const { data, error } = await supabase
@@ -67,4 +68,49 @@ export async function createUrl(
   }
 
   return data;
+}
+
+export async function getRedirect(id: string | undefined) {
+  const { data, error } = await supabase
+    .from("urls")
+    .select("id,primary_url")
+    .or(`short_url.eq.${id}, custom_url.eq.${id}`)
+    .single();
+
+  if (error) {
+    console.log(error.message);
+    throw new Error("Error while redirecting");
+  }
+  return data;
+}
+
+const parser = new UAParser();
+
+export async function getAnalytics({
+  id,
+  primaryUrl,
+}: {
+  id: string;
+  primaryUrl: string;
+}) {
+  try {
+    const res = parser.getResult();
+    const device = res.device.type || "desktop";
+
+    const response = await fetch("https://ipapi.co/json");
+    const { city, country_name: country } = await response.json();
+
+    await supabase.from("analytics").insert({
+      url_id: id,
+      city: city,
+      device: device,
+      country: country,
+    });
+
+    window.location.href = primaryUrl;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
 }
