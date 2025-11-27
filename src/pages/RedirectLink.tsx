4f9e5a95bee097f044a0700431491f1f37a4a431
keshavcoded <keshav.shri06@gmail.com>
2025-11-27 18:41:40 +0530

    feat: redirect not found

--- src/pages/RedirectLink.tsx
+++ src/pages/RedirectLink.tsx
@@ -1,21 +1,34 @@
 import { useFetch } from "@/hooks/useFetch";
 import { getRedirect } from "@/lib/api/url.api";
 import { getAnalytics } from "@/lib/api/analytics.api";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { SkewLoader } from "react-spinners";
+import { useTheme } from "@/components/ThemeProvider";
 
 const RedirectLink = () => {
   const { id } = useParams();
+
+  const { theme } = useTheme();
 
   const { loading, data, fn: redirect } = useFetch(getRedirect);
 
   const { loading: analyticsloader, fn: saveAnalytics } =
     useFetch(getAnalytics);
 
+  const [notFound, setNotFound] = useState<boolean>(false);
+
   useEffect(() => {
     if (!loading) redirect(id);
   }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      if (!data) {
+        setNotFound(true);
+      }
+    }
+  }, [loading, data]);
 
   useEffect(() => {
     if (!loading && data) {
@@ -30,6 +43,20 @@ const RedirectLink = () => {
     return (
       <div className="z-50 inset-0 flex items-center justify-center fixed bg-black/30 bg-blur-md">
         <SkewLoader color="#888" />
+      </div>
+    );
+  }
+
+  if (notFound) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <h1
+          className={`text-md sm:text-xl font-medium ${
+            theme === "dark" ? "text-white " : "text-black"
+          } text-center`}
+        >
+          This link does not exist or has been removed.
+        </h1>
       </div>
     );
   }
